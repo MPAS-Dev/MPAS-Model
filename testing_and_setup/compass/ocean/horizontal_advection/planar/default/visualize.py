febaf270b753c8eeabbbbec940b431b81ec81501
Jamil Gafur <jamilg@gr-fe3.lanl.gov>
2020-01-21 08:57:23 -0700

    updated the output and easier to read

--- testing_and_setup/compass/ocean/horizontal_advection/planar/default/visualize.py
+++ testing_and_setup/compass/ocean/horizontal_advection/planar/default/visualize.py
@@ -12,7 +12,8 @@ import xarray as xr
 import matplotlib.pyplot as plt
 #creates a gif
 import imageio
-
+# creates image directory
+import os
 
 
 def main():
@@ -20,21 +21,29 @@ def main():
   filename = "forward/output/KPP_test.0001-01-01_00.00.00.nc"
   data = xr.open_dataset(filename)
   # set the proper bounds
-  plt.xlim(0,10000)
-  plt.ylim(0,8000)
+  plt.xlim(0,10100)
+  plt.ylim(0,9000)
   gif_frames = data.tracer1.shape[0]
   print("Total Number of Frames to plot: {}".format(gif_frames))
 
+  # make directory to save images in
+  dir_name = "images"
+  os.makedirs(dir_name, exist_ok=True)
+
+
   # cycle through each frame and plot its data and save it as a figure
   for i in range(gif_frames):
+    if (gif_frames - i) % 5 == 0:
+      print("Frames left: {}".format(gif_frames-i))
+
     plt.scatter(data.xCell, data.yCell, c=data.tracer1[i,:,99])
-    plt.savefig(str(i)+".png")
+    plt.savefig(dir_name +"/"+ str(i)+".png")
 
   # holds the imagenames
   images = []
   # cycles through all the frames and saves the data to the array
   for i in range(gif_frames):
-      images.append(imageio.imread(str(i)+".png"))
+      images.append(imageio.imread(dir_name+"/"+str(i)+".png"))
   # creates the gif
   imageio.mimsave('KPP_tracer1.gif', images)    
 
