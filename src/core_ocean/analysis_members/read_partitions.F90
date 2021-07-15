9b906614c8d583b48c26b75c9b6531c0b2ce37ee
Mark Petersen <mpetersen@lanl.gov>
2021-07-15 13:00:22 -0600

    Everything works now. Wrote sample files for Greg

--- src/core_ocean/analysis_members/read_partitions.F90
+++ src/core_ocean/analysis_members/read_partitions.F90
@@ -1,80 +1,82 @@
 ! compile with -fconvert=big-endian  !!!!!
-!  gfortran write_int.F90 -fconvert=big-endian
+! gfortran read_partitions.F90 -fconvert=big-endian
 program read_partition
   implicit none
   integer, parameter :: RKIND  = selected_real_kind(12)
-   real(KIND=RKIND), parameter :: minT = -2.0_RKIND
-   real(KIND=RKIND), parameter :: maxT = 30.0_RKIND
-   real(KIND=RKIND), parameter :: minS =  0.0_RKIND
-   real(KIND=RKIND), parameter :: maxS = 40.0_RKIND
-   real(KIND=RKIND), parameter :: minV = -1.0_RKIND
-   real(KIND=RKIND), parameter :: maxV =  1.0_RKIND
    character(len=6), parameter :: charMPIRank = '000000'
-   character(len=2), parameter :: Hem = 'SH'
+   character(len=2), parameter :: Hem = 'NH'
    character(len=17), parameter :: subdir = 'write_partitions/'
    character(len=10), parameter :: xtime = '0001-01-01'
-   character(len=50) :: filename
-   integer, parameter :: nCells = 206
+   character(len=60) :: filename
+   integer, parameter :: nCells = 203
    integer, parameter :: nVertLevels = 16
    integer(KIND=4), dimension(nCells) :: indexToCellID
-   real(KIND=4), dimension(nCells) :: tempData
-   integer(KIND=1), dimension(nCells) :: maxLevelCell
-   real(KIND=4), dimension(:), allocatable :: R4Col
-   real(KIND=8), dimension(nVertLevels, nCells) :: temperature
+   integer(KIND=4), dimension(nCells) :: maxLevelCell
+   real(KIND=4), dimension(nVertLevels, nCells) :: temperature, salinity
+   real(KIND=4), dimension(nVertLevels, nCells) :: velocityZonal, velocityMeridional, vertVelocityTop
    integer :: iCell, kMax
-   integer, dimension(10) :: varI
-   integer(KIND=1), dimension(10) :: varI1
-   integer(KIND=2), dimension(10) :: varI2
-   integer(KIND=4), dimension(10) :: varI4
-   integer(KIND=8), dimension(10) :: varI8
 
-         filename = 'varI.dat'
+         filename = 'mpaso.indexToCellID.'//Hem//'.p'//charMPIRank//'.I4.dat'
          open(10, file=subdir//filename, status='old', access='stream')
-         read (10) varI
+         read (10) indexToCellID
          close(10)
-         print *, 'varI',varI
-         filename = 'varI1.dat'
-         open(10, file=subdir//filename, status='old', access='stream')
-         read (10) varI1
-         close(10)
-         print *, 'varI1',varI1
-         filename = 'varI2.dat'
-         open(10, file=subdir//filename, status='old', access='stream')
-         read (10) varI2
-         close(10)
-         print *, 'varI2',varI2
-         filename = 'varI4.dat'
-         open(10, file=subdir//filename, status='old', access='stream')
-         read (10) varI4
-         close(10)
-         print *, 'varI4',varI4
-         filename = 'varI8.dat'
-         open(10, file=subdir//filename, status='old', access='stream')
-         read (10) varI8
-         close(10)
-         print *, 'varI8',varI8
+         print *, 'indexToCellID',indexToCellID
 
-         !filename = 'mpaso.indexToCellID.'//Hem//'.p'//charMPIRank//'.I4.dat'
-         !open(10, file=subdir//filename, status='old', access='stream')
-         !read (10) maxLevelCell
-         !close(10)
-         !!print *, 'tempData',tempData
-         !!print *, 'indexToCellID',indexToCellID
+         filename = 'mpaso.maxLevelCell.'//Hem//'.p'//charMPIRank//'.I4.dat'
+         open(10, file=subdir//filename, status='old', access='stream')
+         read (10) maxLevelCell
+         close(10)
+         print *, 'maxLevelCell',maxLevelCell
 
-         !filename = 'mpaso.maxLevelCell.'//Hem//'.p'//charMPIRank//'.I1.dat'
-         !open(10, file=subdir//filename, status='old', access='stream')
-         !read (10) maxLevelCell
-         !close(10)
-         !!print *, 'maxLevelCell',maxLevelCell
+         temperature = -99
+         filename = 'mpaso.temperature.'//xtime(1:10)//'.'//Hem//'.p'//charMPIRank//'.R4.dat'
+         open(10, file=subdir//filename, status='old', access='stream')
+         do iCell = 1, nCells
+            kMax = maxLevelCell(iCell)
+            read (10) temperature(1:kmax,iCell)
+            print *, 'iCell, kmax, temperature',iCell, kmax, temperature(1:kmax,iCell)
+         enddo
+         close(10)
+         print *, 'temperature',temperature(1,1:10)
 
-         !temperature = -99
-         !filename = 'mpaso.temperature.'//xtime(1:10)//'.'//Hem//'.p'//charMPIRank//'.I2.dat'
-         !open(10, file=subdir//filename, status='old', access='stream')
-         !do iCell = 1, nCells
-         !   kMax = maxLevelCell(iCell)
-print *, !iCell, kMax
-         !   read (10) temperature(1:kmax,iCell)
-         !enddo
-         !close(10)
+         salinity = -99
+         filename = 'mpaso.salinity.'//xtime(1:10)//'.'//Hem//'.p'//charMPIRank//'.R4.dat'
+         open(10, file=subdir//filename, status='old', access='stream')
+         do iCell = 1, nCells
+            kMax = maxLevelCell(iCell)
+            read (10) salinity(1:kmax,iCell)
+         enddo
+         close(10)
+         print *, 'salinity',salinity(1,1:10)
+
+         velocityZonal = -99
+         filename = 'mpaso.velocityZonal.'//xtime(1:10)//'.'//Hem//'.p'//charMPIRank//'.R4.dat'
+         open(10, file=subdir//filename, status='old', access='stream')
+         do iCell = 1, nCells
+            kMax = maxLevelCell(iCell)
+            read (10) velocityZonal(1:kmax,iCell)
+         enddo
+         close(10)
+         print *, 'velocityZonal',velocityZonal(1,1:10)
+
+         velocityMeridional = -99
+         filename = 'mpaso.velocityMeridional.'//xtime(1:10)//'.'//Hem//'.p'//charMPIRank//'.R4.dat'
+         open(10, file=subdir//filename, status='old', access='stream')
+         do iCell = 1, nCells
+            kMax = maxLevelCell(iCell)
+            read (10) velocityMeridional(1:kmax,iCell)
+         enddo
+         close(10)
+         print *, 'velocityMeridional',velocityMeridional(1,1:10)
+
+         vertVelocityTop = -99
+         filename = 'mpaso.vertVelocityTop.'//xtime(1:10)//'.'//Hem//'.p'//charMPIRank//'.R4.dat'
+         open(10, file=subdir//filename, status='old', access='stream')
+         do iCell = 1, nCells
+            kMax = maxLevelCell(iCell)
+            read (10) vertVelocityTop(1:kmax,iCell)
+         enddo
+         close(10)
+         print *, 'vertVelocityTop',vertVelocityTop(1,1:10)
 
 end program read_partition
