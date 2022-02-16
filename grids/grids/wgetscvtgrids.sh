f327dde463bc476cdfeda63b4b1b4d6fd7b13cc4
Pedro Peixoto <ppeixoto@usp.br>
2022-02-16 10:44:40 -0300

    added grids folder and script to get grid from ncar site

---
+++ grids/grids/wgetscvtgrids.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+wget http://www2.mmm.ucar.edu/projects/mpas/atmosphere_meshes/x1.2562.tar.gz
+wget http://www2.mmm.ucar.edu/projects/mpas/atmosphere_meshes/x1.10242.tar.gz
+wget http://www2.mmm.ucar.edu/projects/mpas/atmosphere_meshes/x1.40962.tar.gz
+wget http://www2.mmm.ucar.edu/projects/mpas/atmosphere_meshes/x1.163842.tar.gz
+wget http://www2.mmm.ucar.edu/projects/mpas/atmosphere_meshes/x1.655362.tar.gz
+wget http://www2.mmm.ucar.edu/projects/mpas/atmosphere_meshes/x1.2621442.tar.gz
+wget http://www2.mmm.ucar.edu/projects/mpas/atmosphere_meshes/x4.163842.tar.gz
+wget http://www2.mmm.ucar.edu/projects/mpas/atmosphere_meshes/x4.535554.tar.gz
