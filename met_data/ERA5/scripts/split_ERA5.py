8c3b69f9f5d2ed0e0fd3049e91ff02dd4594dc28
daniloceano <danilo.oceano@gmail.com>
2022-10-07 21:14:29 -0300

    fixed split to be just for dates

--- met_data/ERA5/scripts/split_ERA5.py
+++ met_data/ERA5/scripts/split_ERA5.py
@@ -3,8 +3,8 @@
 """
 Created on Fri Oct  7 15:45:55 2022
 
-Split GRIB or NetCDF file: creates new files, each one containing a single
-    variable, for a single timestep of the model.
+Split GRIB or NetCDF file: creates new files, each one for a single timestep 
+    of the model.
 
 @author: Danilo Couto de Souza
 """
@@ -14,31 +14,38 @@ from cfgrib.xarray_to_grib import to_grib
 import pandas as pd 
 import os
 import sys
+import warnings
 
+# supress the "GRIB write support is experimental" warning
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+# provide grib file as argument
 file = sys.argv[1]
-
 print('opening file: '+file+'...')
-
-ds = xr.open_dataset(file)
-
+# open grib file
+ds = xr.open_dataset(file, engine='cfgrib')
 print('ok')
 
+# get prefix
 name = os.path.basename(file).split('.')[0]
 coords = ds.coords
 
-for var in ds.variables:
-    print('exporting variable: '+var+'...')
-    if var not in coords:
-        for ts in ds.time:
+# for var in ds.variables:
+#     print('exporting variable: '+var+'...')
+#     if var not in coords:
+for ts in ds.time:
             
-            tmp = ds[var].sel(time=ts)
-            
-            t = pd.to_datetime(str(ts.values)) 
-            d = t.strftime('%Y-%m-%d-%H%M')
-            
-            file_string = 'ERA5_'+var+'_'+d+'.grib'
-            ds2 = tmp.to_dataset(name=var)
-        
-            print('saving file: '+file_string+'...')
-            to_grib(ds2, file_string)
-            print(file_string+' saved')
+    # tmp = ds[var].sel(time=ts)
+    tmp = ds.sel(time=ts)
+    
+    t = pd.to_datetime(str(ts.values)) 
+    d = t.strftime('%Y-%m-%d-%H%M')
+    
+    # file_string = 'ERA5_'+var+'_'+d+'.grib'
+    file_string = 'ERA5_'+d+'.grib'
+    # ds2 = tmp.to_dataset(name=var)
+    # ds2 = tmp.to_dataset(name=d)
+
+    print('saving file: '+file_string+'...')
+    to_grib(ds, file_string)
+    print(file_string+' saved~')
