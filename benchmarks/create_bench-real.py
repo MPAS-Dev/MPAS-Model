12f8803a5acf59dab937938f12c51ddaf664c58d
daniloceano <danilo.oceano@gmail.com>
2022-10-17 19:57:42 -0300

    fix

--- benchmarks/create_bench-real.py
+++ benchmarks/create_bench-real.py
@@ -290,9 +290,9 @@ for par1, par2 in itertools.product(loop_parameter1, loop_parameter2):
         b_init = bench.Bench(args, dummy_string=" Pars:"+str(par1)+" - "+str(par2))
         b_init.set_options(nml_opts, str_opt, b_full_name)
 
-        shutil.copy(work_dir+"/benchmarks/inputs/stream_list.atmosphere.diagnostics", b_full_name+"/stream_list.atmosphere.diagnostics")
-        shutil.copy(work_dir+"/benchmarks/inputs/stream_list.atmosphere.output", b_full_name+"/stream_list.atmosphere.output")
-        shutil.copy(work_dir+"/benchmarks/inputs/stream_list.atmosphere.surface", b_full_name+"/stream_list.atmosphere.surface")
+        shutil.copy(work_dir+"/default_inputs/stream_list.atmosphere.diagnostics", b_full_name+"/stream_list.atmosphere.diagnostics")
+        shutil.copy(work_dir+"/default_inputs/stream_list.atmosphere.output", b_full_name+"/stream_list.atmosphere.output")
+        shutil.copy(work_dir+"/default_inputs/stream_list.atmosphere.surface", b_full_name+"/stream_list.atmosphere.surface")
 
         print("Benchmark dir:", b_full_name)
 
