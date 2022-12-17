65ef116b502bab1fb4f226b7ee5120faa26941c0
daniloceano <danilo.oceano@gmail.com>
2022-12-16 23:17:18 -0300

    trying to fix legends and figure proportions

--- validate/validate_INMET-benchmarks-microp_convection.py
+++ validate/validate_INMET-benchmarks-microp_convection.py
@@ -277,7 +277,7 @@ variables = ['temperature','precipitation','windspeed','pressure']
 plt.close('all')
 fig, axes = plt.subplots(4, 3, figsize=(25, 13))
 fig.tight_layout()
-plt.subplots_adjust(left=0.02,bottom=0.02,wspace=1.5)
+plt.subplots_adjust(left=0.05,bottom=0.05,wspace=1.5)
 i,v = 1,0
 for row in range(4):
     # One variable for each row
