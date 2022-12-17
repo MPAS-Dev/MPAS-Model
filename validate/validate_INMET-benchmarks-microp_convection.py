a2917f550ba51c5a3c814a14939ffc95c16f58c7
daniloceano <danilo.oceano@gmail.com>
2022-12-17 00:24:26 -0300

    trying to fix figure proportions

--- validate/validate_INMET-benchmarks-microp_convection.py
+++ validate/validate_INMET-benchmarks-microp_convection.py
@@ -275,9 +275,9 @@ with open(station_file, 'r',encoding='latin-1') as file:
 met_list = []
 variables = ['temperature','precipitation','windspeed','pressure']
 plt.close('all')
-fig, axes = plt.subplots(4, 3, figsize=(25, 15))
-fig.tight_layout()
+fig, axes = plt.subplots(4, 3, figsize=(30, 15))
 plt.subplots_adjust(left=0.04,bottom=0.08,top=0.95, wspace=2, hspace=0.5)
+fig.tight_layout()
 i,v = 1,0
 for row in range(4):
     # One variable for each row
