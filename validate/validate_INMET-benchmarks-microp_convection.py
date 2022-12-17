9b06e1b3e2f5175e7a9d5f09805e9411265b63e2
daniloceano <danilo.oceano@gmail.com>
2022-12-16 23:45:29 -0300

    trying to fix legends and figure proportions

--- validate/validate_INMET-benchmarks-microp_convection.py
+++ validate/validate_INMET-benchmarks-microp_convection.py
@@ -219,7 +219,7 @@ def plot_qq(data,ax):
         
         g = sns.regplot(x=reference, y=predicted, data=data, label=experiment,
                         ax=ax)
-    ax.legend(loc='upper center', fontsize=10, bbox_to_anchor=(2.5, 1),ncol=3)
+    ax.legend(loc='upper center', fontsize=14, bbox_to_anchor=(2.5, 1),ncol=3)
     g.set_ylabel('EXPERIMENT')
     g.set_xlabel('INMET')
     
@@ -277,8 +277,8 @@ variables = ['temperature','precipitation','windspeed','pressure']
 plt.close('all')
 fig, axes = plt.subplots(4, 3, figsize=(28, 13))
 fig.tight_layout()
-plt.subplots_adjust(left=0.05,bottom=0.05,top=0.98, wspace=2.2)
-# sns.set(font_scale = 6)
+plt.subplots_adjust(left=0.05,bottom=0.05,top=0.98, wspace=1.5)
+plt.rcParams.update({'font.size': 16})
 i,v = 1,0
 for row in range(4):
     # One variable for each row
@@ -327,7 +327,7 @@ for row in range(4):
                              style='microp', hue='cumulus',
                              markers=True,
                          ax=axes[row,col],data=data)
-            axes[row,col].legend(loc='upper center',fontsize=10,
+            axes[row,col].legend(loc='upper center',fontsize=14,
                                  bbox_to_anchor=(1.5, 1),ncol=2)
             axes[row,col].set(ylabel=variable, xlabel=None)
         # Plot taylo diagrams in the second columns
