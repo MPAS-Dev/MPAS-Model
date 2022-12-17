092d9662bad08e1ff6cc5e93b105d0b73694ee39
daniloceano <danilo.oceano@gmail.com>
2022-12-16 22:48:43 -0300

    trying to fix legends

--- validate/validate_INMET-benchmarks-microp_convection.py
+++ validate/validate_INMET-benchmarks-microp_convection.py
@@ -219,10 +219,10 @@ def plot_qq(data,ax):
         
         g = sns.regplot(x=reference, y=predicted, data=data, label=experiment,
                         ax=ax)
+    legend = g.legend(fontsize=20, ncol=3)
     sns.move_legend(g, "upper left", bbox_to_anchor=(1, 1))
     g.set_ylabel('EXPERIMENT',fontsize=16)
     g.set_xlabel('INMET',fontsize=16)
-    legend = g.legend(fontsize=20, ncol=3)
     frame = legend.get_frame()
     frame.set_color('white')
     
