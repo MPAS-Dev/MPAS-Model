c5667d264499773e00c35797cd345b4293bca7d4
daniloceano <danilo.oceano@gmail.com>
2023-01-09 14:19:49 -0300

    fixing figure sizes

--- validate/validate_INMET-benchmarks-microp_convection.py
+++ validate/validate_INMET-benchmarks-microp_convection.py
@@ -400,6 +400,7 @@ plt.suptitle('Station: '+station+"\nStation lat, lon, z: "+
              str(round(z_station,2))+"\nModel lat, lon, z: "+ str(lat)+
              ", "+str(lon)+", "+str(z),fontsize=22)
 # plt.tight_layout(h_pad=10)
+plt.subplots_adjust(hspace=0.2,wspace=0.4)
 plt.savefig(outdir+fname+'_taylor-diagram_'+station+'.png', dpi=300)
 print(fname+'_taylor-diagram_'+station+'.png created!')
 
@@ -414,7 +415,7 @@ plt.suptitle('Station: '+station+"\nStation lat, lon, z: "+
              str(round(lat_station,2))+", "+str(round(lon_station,2))+", "+
              str(round(z_station,2))+"\nModel lat, lon, z: "+ str(lat)+
              ", "+str(lon)+", "+str(z),fontsize=22, y=0.98)
-plt.subplots_adjust(hspace=0.4,wspace=0.45,right=0.7, top=0.83)
+plt.subplots_adjust(hspace=0.5,wspace=0.45,right=0.7, top=0.83)
 plt.savefig(outdir+fname+'_qq-plots_'+station+'.png',
             bbox_inches='tight', dpi=300)
 print(fname+'_qq-plots_'+station+'.png created!')
