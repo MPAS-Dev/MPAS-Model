e11c1a695d6d2d61c5649901230a0c32627b190c
daniloceano <danilo.oceano@gmail.com>
2023-01-09 14:27:01 -0300

    fixing figure sizes

--- validate/validate_INMET-benchmarks-microp_convection.py
+++ validate/validate_INMET-benchmarks-microp_convection.py
@@ -387,7 +387,7 @@ plt.savefig(outdir+fname+'_timeseries_'+station+'.png', dpi=300)
 print(fname+'_timeseries_'+station+'.png created!')
 
 ## Plot Taylor Diagrams ##
-fig = plt.figure(figsize=(20,10))
+fig = plt.figure(figsize=(30,15))
 for i in range(6):
     variable = variables[i]
     var_data = data[data['variable'] == variable]
@@ -399,8 +399,8 @@ plt.suptitle('Station: '+station+"\nStation lat, lon, z: "+
              str(round(lat_station,2))+", "+str(round(lon_station,2))+", "+
              str(round(z_station,2))+"\nModel lat, lon, z: "+ str(lat)+
              ", "+str(lon)+", "+str(z),fontsize=22)
-# plt.tight_layout(h_pad=10)
-plt.subplots_adjust(hspace=0.4,wspace=0.8)
+plt.tight_layout(w_pad=10)
+# plt.subplots_adjust(hspace=0.4,wspace=0.8)
 plt.savefig(outdir+fname+'_taylor-diagram_'+station+'.png', dpi=300)
 print(fname+'_taylor-diagram_'+station+'.png created!')
 
