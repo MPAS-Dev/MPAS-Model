628f6bf277af02bf836f999576fa0207a38a318b
daniloceano <danilo.oceano@gmail.com>
2023-05-04 14:59:57 -0300

    Feat: script for running benchmarks for pbl excluding the ones that were already done running

---
+++ benchmarks/run_bench-real_pbl-test.py
@@ -0,0 +1,73 @@
+#-------------------------------------------------
+#  Script to generate namelists and stream for MPAS
+#  Author: P. Peixoto <ppeixoto@usp.br>
+#  Last update: Mar 2022
+# --------------------------------------------------
+import os
+import sys
+import subprocess
+import glob
+import time 
+import mpas_benchmarks_RealCase as bench
+
+# Get args: init or run core
+args = bench.call_parser()
+
+#Workspace
+work_dir = os.getenv('MPAS_DIR')
+if work_dir is None:
+    print('MPAS_DIR not on environment! Set it and try again')
+    sys.exit(1)
+b_name = args.name
+b_dir = work_dir+"/benchmarks/"+args.bdir
+print(b_dir)
+
+#Init_atmosphere setup
+if args.run:
+    #Make sure the init test exists!
+    benchs = glob.glob(b_dir+"/run.*") 
+    benchs.remove("./run.best-physics_sst.mp_thompson.cu_ntiedtke.bl_ysu")
+    benchs.remove("run.best-physics_sst.mp_thompson.cu_ntiedtke.bl_mynn")
+    mainexec = "mpiexec -n "+str(args.threads)+" ./atmosphere_model"
+else:
+    benchs = glob.glob(b_dir+"/init*")
+    mainexec = "./init_atmosphere_model"
+
+
+print("---------------------------")
+print(" Running :", benchs)
+print("---------------------------")
+for b in benchs:
+    print()
+    print(" Running :", b)
+    print()
+    
+    # Time model run: get initial time 
+    start_time = time.time()
+    
+    p = subprocess.Popen(mainexec, stdout = subprocess.PIPE,
+                          stderr=subprocess.PIPE, cwd=b, shell=True)
+
+    time.sleep(5)
+    try:
+        outfile = bench.find("log.*out", b)[0]  
+        print(outfile)   
+        follow = bench.LogFollower(open(outfile))
+
+        while p.poll() is None:
+            for line in follow:
+                print(line)
+    except:
+        print("Couldn't reach log file, check manually, just in case")   
+    
+    # Duration of model run 
+    lenght = (time.time() - start_time)/60/60
+    print("--- %s hours for running model ---" % (lenght))
+    
+    # Write time required for model run into text file
+    f = open(b+'/run_duration', 'w' )
+    f.write(str(lenght)+' hours')
+    f.close()
+    
+    # wait process to finish before starting a new one    
+    p.wait()    
